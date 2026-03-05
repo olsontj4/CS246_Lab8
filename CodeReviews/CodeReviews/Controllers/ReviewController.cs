73995d07ac6fa16d0d90b72de6631408ec52b56e
Thomas Olson <thomas.josholson@gmail.com>
2026-03-04 17:43:42 -0800

    ReviewController fix.

--- CodeReviews/CodeReviews/Controllers/ReviewController.cs
+++ CodeReviews/CodeReviews/Controllers/ReviewController.cs
@@ -37,6 +37,38 @@ namespace CodeReviews.Controllers
         {
             review.ReviewDate = DateTime.Now;
 
+            // TODO: Remove this dummy submission after Submission entry code is done
+            review.Submission = new Submission
+            {
+                CodeUrl = review.Submission?.CodeUrl ?? "",
+                Version = "A",
+                SubmissionDate = DateTime.Now,
+                Student = new AppUser { Name = "Dummy Student" },
+                Assignment = new Assignment
+                {
+                    AssignmentName = "Dummy Assignment",
+                    DraftDueDate = DateOnly.FromDateTime(DateTime.Now),
+                    ReviewDueDate = DateOnly.FromDateTime(DateTime.Now),
+                    FinalDueDate = DateOnly.FromDateTime(DateTime.Now),
+                    ClassSection = new Section
+                    {
+                        SectionNumber = 0,
+                        Day = "TBD",
+                        StartTime = "TBD",
+                        Modality = "TBD",
+                        Term = "TBD",
+                        Year = DateTime.Now.Year,
+                        Course = new Course
+                        {
+                            CoursePrefix = "CS",
+                            CourseNumber = "000",
+                            CourseName = "Dummy Course"
+                        },
+                        Instructor = new AppUser { Name = "Dummy Instructor" }
+                    }
+                }
+            };
+
             if (repo.AddReview(review) == 1)
             {
                 return RedirectToAction("List");
